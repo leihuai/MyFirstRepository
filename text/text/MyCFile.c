8e87719e19352ad61e830e150e03702ad75bd563
王磊怀 <leihuaiquwo@163.com>
2016-12-14 15:19:18 +0800

    我在这里又创建了两个文件

---
+++ text/text/MyCFile.c
@@ -0,0 +1,9 @@
+//
+//  MyCFile.c
+//  text
+//
+//  Created by 王磊怀 on 16/12/14.
+//  Copyright © 2016年 王磊怀. All rights reserved.
+//
+
+#include "MyCFile.h"
---
+++ text/text/MyCFile.h
@@ -0,0 +1,14 @@
+//
+//  MyCFile.h
+//  text
+//
+//  Created by 王磊怀 on 16/12/14.
+//  Copyright © 2016年 王磊怀. All rights reserved.
+//
+
+#ifndef MyCFile_h
+#define MyCFile_h
+
+#include <stdio.h>
+
+#endif /* MyCFile_h */
